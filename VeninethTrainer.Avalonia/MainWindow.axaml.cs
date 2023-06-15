056e727b3c745b586e40b176d7a456ceed6e159e
StarShadow <oparkerj@gmail.com>
2023-06-14 21:39:00 -0700

    Create Avalonia project from template

---
+++ VeninethTrainer.Avalonia/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace VeninethTrainer.Avalonia;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
